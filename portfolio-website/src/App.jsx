37f62326b4f20051a99afcbe9f9ea4baa01af615
Martin Wolf <martinwolflauring@gmail.com>
2024-04-04 09:01:07 +0200

    Navbar finished

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {About, Footer, Header, Skills, Testimonial, Work} from './container'
+import { Navbar } from './components/navbar'
+
+function App() {
+
+  return (
+    <div className='app flex flex-col items-center'>
+      <Navbar/>
+      {/* <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonial/>
+      <Footer/> */}
+
+    </div>
+    
+  )
+}
+
+export default App
