4ed662e01f86fdb674e5c9a789909389f162c74e
Martin Wolf <martinwolflauring@gmail.com>
2024-04-11 12:10:36 +0200

    Final commit

--- portfolio-website/src/components/socialmedia.jsx
+++ portfolio-website/src/components/socialmedia.jsx
@@ -5,17 +5,17 @@ import resume from '../assets/resume.pdf'
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div className="social-icon p-text" data-tooltip="GitHub">
+    <div className="social-icon icon-1 p-text" data-tooltip="GitHub">
       <a href="https://github.com/MWolf02" target="_blank">
         <BsGithub />
       </a>
     </div>
-    <div className="social-icon p-text" data-tooltip="LinkedIn">
+    <div className="social-icon icon-2 p-text" data-tooltip="LinkedIn">
       <a href="https://www.linkedin.com/in/martin-wolf-lauring-370780207/" target="_blank">
         <BsLinkedin />
       </a>
     </div>
-    <div className="social-icon p-text" data-tooltip="Resume">
+    <div className="social-icon icon-3 p-text" data-tooltip="Resume">
       <a href={resume} download>
         <FaFileAlt />
       </a>
